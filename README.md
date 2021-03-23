eb303d823a8534e98ce2dd4008260df5aedc5394
brysonS <brys1saclaus2@gmail.com>
2021-03-23 17:22:16 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HawaiiBeaches
-A side project app made to practice JSON Codable. 
+A side project app made to practice JSON Codable, Networking, etc.
+API used: https://hawaiibeachsafety.com/api
+
---
+++ iOSHawaiiBeaches/iOSHawaiiBeaches/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSHawaiiBeaches
+//
+//  Created by BrysonSaclausa on 3/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Hello World 
+    }
+
+
+}
+
